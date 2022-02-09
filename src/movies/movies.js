21cd5c7df35472e731a066847562653557e6c3bf
jordydp <jdepril@outlook.com>
2022-02-09 14:37:55 +0100

    Creating Genres for ea movie

--- src/movies/movies.js
+++ src/movies/movies.js
@@ -9,28 +9,68 @@ const getPop = () =>{
     .then(data => {
         for(let i = 0; i < 5; i++){
             let groupData = data.results[i];
-            console.log(groupData);
-            cardCreater(groupData,i);
-           
+            //console.log(groupData);
+            getMovie(groupData);       
         };
         
     }
     )
     };
 
+const getMovie = (groupData) =>{
+    const movieId = groupData.id;
+    fetch("https://api.themoviedb.org/3/movie/" + movieId + "?api_key="+ data.key)
+    .then(response => response.json())
+    .then(info =>{
+        const information = info.genres;
+        //console.log(information);
+        cardCreater(groupData,information);
+        return information;
+        
+    })
 
+};
 
-const cardCreater = (groupData,i) =>{
-    console.log(groupData);
-    
+const cardCreater = (groupData,genreList) =>{
+    console.log(genreList)
+
     const getWrapper = document.querySelector(".wrapper");
     const makeCard = document.createElement("div");
     makeCard.classList.add("card");
     getWrapper.appendChild(makeCard);
+
     const makeImg = document.createElement("img");
     makeImg.src="https://image.tmdb.org/t/p/original/" + groupData.poster_path;
     makeCard.appendChild(makeImg);
 
+    const makeDescription = document.createElement("div");
+    makeDescription.classList.add("descriptions");
+    makeCard.appendChild(makeDescription);
+
+    const makeMovieTitle = document.createElement("h1");
+    makeMovieTitle.innerText = groupData.title;
+    makeDescription.appendChild(makeMovieTitle);
+
+    const makeReleaseDate = document.createElement("h4");
+    makeReleaseDate.innerText = groupData.release_date;
+    makeDescription.appendChild(makeReleaseDate);
+
+    /*let genresTitle = getMovie();
+    console.log(genresTitle);*/
+    for(let i = 0; i < genreList.length ; i++){
+    const makeGenres = document.createElement("h4");
+    makeGenres.innerHTML = genreList[i].name;
+    makeDescription.appendChild(makeGenres);
+    }
+
+    const makeSummary = document.createElement("p");
+    makeSummary.innerText = groupData.overview;
+    makeDescription.appendChild(makeSummary);
+
+
+
+
 };
 
 getPop();
+getMovie();
