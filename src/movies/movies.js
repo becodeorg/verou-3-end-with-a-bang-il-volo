6396f74360f4809da475f986ab542fd1d4457196
jordydp <jdepril@outlook.com>
2022-02-10 18:29:53 +0100

    Fetch two types of movielists

--- src/movies/movies.js
+++ src/movies/movies.js
@@ -1,86 +1,49 @@
 import data from "../../config/config.js";
-const apiUrl = "https://api.themoviedb.org/3/movie/76341?api_key=";
+
 const apiPop = "https://api.themoviedb.org/3/movie/popular?api_key=";
+const apiTop = "https://api.themoviedb.org/3/movie/top_rated?api_key=";
 
+const getFromApi = (apiUrl, sectionKey) => {
+    const section = document.getElementById(sectionKey);
+    
+    fetch(apiUrl + data.key)
+        .then(response => response.json())
+        .then(data => {
+            for (let i = 0; i < 10; i++) {
+                let groupData = data.results[i];
+                //console.log(groupData);
+                getMovie(section, groupData, i);
+            };
 
-const slider = document.querySelector('.container-movies');
-let isDown = false;
-let startX;
-let scrollLeft;
+        })
+};
 
-slider.addEventListener('mousedown', e => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener('mouseleave', _ => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-slider.addEventListener('mouseup', _ => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-slider.addEventListener('mousemove', e => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const SCROLL_SPEED = 3;
-  const walk = (x - startX) * SCROLL_SPEED;
-  slider.scrollLeft = scrollLeft - walk;
-});
-
-
-const getPop = () =>{
-    fetch(apiPop + data.key)
-    .then(response => response.json())
-    .then(data => {
-        for(let i = 0; i < 10 ; i++){
-            let groupData = data.results[i];
-            //console.log(groupData);
-            getMovie(groupData);       
-        };
-        
-    }
-    )
-    };
-
-const getMovie = (groupData) =>{
+const getMovie = (section, groupData, i) => {
     const movieId = groupData.id;
-    fetch("https://api.themoviedb.org/3/movie/" + movieId + "?api_key="+ data.key)
-    .then(response => response.json())
-    .then(info =>{
-        const information = info.genres;
-        const movieDuration = info.runtime;
-        //console.log(movieDuration);
-        //console.log(information);
-        cardCreater(groupData,information,movieDuration);
-        timeConvert(movieDuration);
-        return [information, movieDuration];
-        
-    })
+    fetch("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + data.key)
+        .then(response => response.json())
+        .then(info => {
+            const information = info.genres;
+            const movieDuration = info.runtime;
+            //console.log(movieDuration);
+            //console.log(information);
+            cardCreater(section, groupData, information, movieDuration, i);
+            timeConvert(movieDuration);
+            return [information, movieDuration];
+
+        })
 
 };
 
-const timeConvert = (duration) => {
-    var num = duration;
-    var hours = (num / 60);
-    var rhours = Math.floor(hours);
-    var minutes = (hours - rhours) * 60;
-    var rminutes = Math.round(minutes);
-    return num + " minutes = " + rhours + " hour(s) and " + rminutes + " minute(s).";
-    }
+const cardCreater = (section, groupData, genreList, duration, ) => {
 
-const cardCreater = (groupData,genreList,duration) =>{
-    
-    const getWrapper = document.querySelector(".container-movies");
+    const getWrapper = section.getElementsByClassName("container-movies")[0];
     const makeCard = document.createElement("div");
     makeCard.classList.add("box");
     getWrapper.appendChild(makeCard);
 
     const makeImg = document.createElement("img");
-    makeImg.src="https://image.tmdb.org/t/p/original/" + groupData.poster_path;
+    makeImg.src = "https://image.tmdb.org/t/p/original/" + groupData.poster_path;
     makeCard.appendChild(makeImg);
 
     const makeDescription = document.createElement("div");
@@ -101,10 +64,10 @@ const cardCreater = (groupData,genreList,duration) =>{
     const makeGenrelist = document.createElement("div");
     makeGenrelist.classList.add("genrelist");
     makeDescription.appendChild(makeGenrelist);
-    for(let i = 0; i < genreList.length ; i++){
-    const makeGenres = document.createElement("h4");
-    makeGenres.innerHTML = genreList[i].name;
-    makeGenrelist.appendChild(makeGenres);
+    for (let i = 0; i < genreList.length; i++) {
+        const makeGenres = document.createElement("h4");
+        makeGenres.innerHTML = genreList[i].name;
+        makeGenrelist.appendChild(makeGenres);
     }
 
     const makeSummary = document.createElement("p");
@@ -112,11 +75,11 @@ const cardCreater = (groupData,genreList,duration) =>{
     makeDescription.appendChild(makeSummary);
 
     const makeATag = document.createElement("a");
-    makeATag.href= "https://www.themoviedb.org/movie/"+ groupData.id;
+    makeATag.href = "https://www.themoviedb.org/movie/" + groupData.id;
     makeATag.target = "_blank"
     makeATag.innerText = "Read More...";
     makeDescription.appendChild(makeATag);
-    
+
     tippy(makeCard, {
         placement: "right",
         interactive: true,
@@ -124,11 +87,7 @@ const cardCreater = (groupData,genreList,duration) =>{
         theme: "light-border",
         content: makeDescription,
     })
-
-
-
-
 };
+getFromApi(apiPop,"popular");
+getFromApi(apiTop,"top_rated");
 
-getPop();
-getMovie();
